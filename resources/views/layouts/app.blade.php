9c82910b7457bb74ec23e4279d3a95b73fbdfc3d
farhandardiri <faran.f4124n@gmail.com>
2021-04-24 16:09:15 +0700

    add file informationController, information model , database model & edit beberapa file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,26 +49,7 @@
 
                 <div class="content-wrapper">
                     <!-- Content Header (Page header) -->
-                    <div class="content-header">
-                      <div class="container-fluid">
-                        <div class="row mb-2">
-                          <div class="col-sm-6">
-                              <ul class="navbar-nav">
-                                  <li class="nav-item">
-                                    <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-                                  </li>
-                              </ul>
-                            <h1 class="m-0 text-dark">Dashboard v2</h1>
-                          </div><!-- /.col -->
-                          <div class="col-sm-6">
-                            <ol class="breadcrumb float-sm-right">
-                              <li class="breadcrumb-item"><a href="#">Home</a></li>
-                              <li class="breadcrumb-item active">Dashboard v2</li>
-                            </ol>
-                          </div><!-- /.col -->
-                        </div><!-- /.row -->
-                      </div><!-- /.container-fluid -->
-                    </div>
+
 
                     {{ $slot }}
                 <!-- Content Wrapper. Contains page content -->
@@ -76,7 +57,7 @@
                 <!-- /.content-wrapper -->
 
                 <!-- /.control-sidebar -->
-
+            </section>
                 <!-- Main Footer -->
                 <footer class="main-footer">
                   <strong>Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong>
--- resources/views/name.blade.php
+++ resources/views/name.blade.php
@@ -1,18 +1,57 @@
 <x-app-layout>
     <x-slot name="slot">
 
-            <!-- /.content-header -->
+        <div class="content-header">
+            <div class="container-fluid">
+              <div class="row mb-2">
+                <div class="col-sm-6">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                          <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+                        </li>
+                    </ul>
+                  <h1 class="m-0 text-dark">Insert Infromation</h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                  <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    <li class="breadcrumb-item active">Dashboard v2</li>
+                  </ol>
+                </div><!-- /.col -->
+              </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+          </div>
 
-            <!-- Main content -->
 
-                        <h2>Test1 open</h2>
+          <section class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h3>Give Infromation</h3>
+                    </div>
+                    <div class="card-body">
+                        <form>
+                            <div class="form-row">
+                              <div class="form-group col-md-6">
+                                <input type="text" class="form-control" placeholder="First name">
+                              </div>
+                              <div class="form-group col-md-6">
+                                <input type="text" class="form-control" placeholder="Last name">
+                              </div>
 
-                  <!-- /.col -->
+                              <div class="form-group col-md-6">
+                                <input type="file" class="form-control" placeholder="Last name">
+                              </div>
 
-                <!-- /.row -->
+                            </div>
+                            <button type="submit" class="btn btn-primary"> Submit </button>
+                          </form>
+                    </div>
 
-            </section>
-            <!-- /.content -->
+                </div>
+
+          </section>
+
+
           </div>
     </x-slot>
 
